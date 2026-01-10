2fb1e3429dfb92d5260f3e982171beee5cc8f230
Victor Emmanuel <ndukwovictor3@gmail.com>
2026-01-10 18:04:10 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,3 +1,4 @@
+
 import { OnboardingForm } from './OnboardingForm';
 import { Logo } from '@/components/Logo';
 import Link from 'next/link';
@@ -6,9 +7,7 @@ export default function OnboardingPage() {
   return (
     <div className="container mx-auto flex min-h-screen max-w-lg flex-col items-center p-4">
       <header className="w-full py-4">
-        <Link href="/" className="text-2xl font-bold font-headline">
-            <Logo />
-        </Link>
+        <Logo className="text-2xl font-bold font-headline" />
       </header>
       <main className="flex w-full flex-1 flex-col justify-center">
         <OnboardingForm />
