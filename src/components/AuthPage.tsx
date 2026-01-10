087e822e62874f706dc62d3addae7a0bdc9bc778
Victor Emmanuel <ndukwovictor3@gmail.com>
2026-01-10 17:45:11 +0000

    whats wrong with you . at the signup place, i cant see a place for sign

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -23,15 +23,6 @@ type UserData = {
     email: string;
 };
 
-const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="24px" height="24px" {...props}>
-        <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0,0,0,0,0,0l6.19,5.238C39.608,34.09,44,28.866,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
-        <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
-        <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.223,0-9.651-3.358-11.297-7.962l-6.571,4.819C9.656,39.663,16.318,44,24,44z" />
-        <path fill="#1565C0" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571l6.19,5.238C43.021,36.25,44,30.413,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
-    </svg>
-);
-  
 export function AuthPage({ defaultTab }: { defaultTab: "login" | "signup" }) {
     const [authStep, setAuthStep] = useState<AuthStep>(defaultTab);
     const router = useRouter();
@@ -128,7 +119,7 @@ export function AuthPage({ defaultTab }: { defaultTab: "login" | "signup" }) {
                     </button>
                 </div>
 
-                <div className="relative mt-6 h-[280px] overflow-hidden">
+                <div className="relative mt-6 h-[340px] overflow-hidden">
                     <AnimatePresence mode="wait">
                         <motion.div
                             key={authStep}
