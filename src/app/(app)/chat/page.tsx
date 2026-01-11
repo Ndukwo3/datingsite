bd67b116572955e7c67da72cbdd1ca8ae0c0a7c7
Victor Emmanuel <ndukwovictor3@gmail.com>
2026-01-11 09:23:52 +0000

    the chat profile img not well aligned (_for element img_)

--- src/app/(app)/chat/page.tsx
+++ src/app/(app)/chat/page.tsx
@@ -1,3 +1,4 @@
+
 import Link from 'next/link';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
